4e3cc75ab285c1066e49626e7e6459f377b79ac7
safiraauliafr <safiraafr@gmail.com>
2022-02-21 15:29:54 +0700

    modified

--- fetchData.py
+++ fetchData.py
@@ -1,14 +1,24 @@
 from pprint import pprint
 import requests
+from assertpy import assert_that
 
 responseFetch = requests.get("https://airportgap.dev-tester.com/api/airports/")
 print(responseFetch.status_code)
 # print(responseFetch.json())
 
-#pprint(responseFetch.json())
-pprint(len(responseFetch.json().get("data")))
+status = responseFetch.status_code
+print(type(status))
 
-assert len(responseFetch.json().get("data")) > 1
+if status == 200:
+    # print('True')
+    pprint(responseFetch.text)
+    pprint(len(responseFetch.json().get("data")))
+    assert len(responseFetch.json().get("data")) > 1
+
+else:
+    print('False')     
+
+# if status code is equal to 200, then print the data
 
 # pprint is used for wrap the json result
 
